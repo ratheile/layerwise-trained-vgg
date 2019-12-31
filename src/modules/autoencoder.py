c2e8ebd8576f84bb455399b4f7a7ee8a20f87650
Raffael Theiler <rptheiler@protonmail.com>
2019-12-31 14:40:53 +0100

    add missing parts

--- src/modules/autoencoder.py
+++ src/modules/autoencoder.py
@@ -33,6 +33,64 @@ class StackableNetwork(object):
     raise "Provide an upstream function"
   
 
+class Autoencoder(nn.Module, StackableNetwork):
+
+  def __init__(self, color_channels: int=3, dropout: int=0.3):
+    super().__init__()
+
+
+    bn_args = { "eps":1e-05, "momentum":0.1, "affine":True, "track_running_stats":True }
+    c2d_args = { "kernel_size":3, "stride":1, "padding":1 }
+
+
+    # Conv2d:      b,1c,w,h       -->  b,8c,w,h
+    # MaxPool2d:   b,8c,w,h       -->  b,8c,w/2,h/2
+    # Conv2d:      b,8c,w/2,h/2   -->  b,16c,w/2,h/2
+    # MaxPool2d:   b,16c,w/2,h/2  -->  b,16c,w/4,h/4
+    self.upstream_layers = nn.Sequential( 
+      nn.Conv2d(in_channels=color_channels, out_channels=color_channels*8, **c2d_args),   
+      nn.BatchNorm2d(num_features=color_channels*8, **bn_args),
+      nn.Dropout(dropout),
+      nn.LeakyReLU(True),
+      nn.MaxPool2d(kernel_size=2),  
+    )
+
+    self.encoder = nn.Sequential(
+      nn.Conv2d(in_channels=color_channels*8, out_channels=color_channels*16, **c2d_args), 
+      nn.BatchNorm2d(num_features=color_channels*16, **bn_args),
+      nn.Dropout(0.3),
+      nn.LeakyReLU(True),
+      nn.MaxPool2d(kernel_size=2)   
+    )
+
+    # Interpolate:  b,16c,w/4,h/4  -->  b,16c,w/2,h/2
+    # Conv2d:       b,16c,w/2,h/2  -->  b,8c,w/2,h/2
+    # Interpolate:  b,8c,w/2,h/2   -->  b,8c,w,h
+    # Conv2d:       b,8c,w,h       -->  b,1c,w,h
+    self.final_conv2d = nn.Conv2d(in_channels=color_channels*8, out_channels=color_channels*1, **c2d_args)
+
+    self.decoder = nn.Sequential(
+      Interpolate(),                
+      nn.Conv2d(in_channels=color_channels*16, out_channels=color_channels*8, **c2d_args),       
+      nn.BatchNorm2d(num_features=color_channels*8, **bn_args),
+      nn.LeakyReLU(True),
+      Interpolate(),                
+      self.final_conv2d,   
+      nn.BatchNorm2d(num_features=color_channels*1, **bn_args),
+      nn.Tanh()
+    )
+
+  def calculate_upstream(self, x):
+    x = self.upstream_layers(x)
+    return x
+
+  def forward(self, x):
+    x = self.upstream_layers(x)
+    x = self.encoder(x)
+    x = self.decoder(x)
+    return x
+  
+
 class SidecarAutoencoder(nn.Module):
   def __init__(self, 
   main_network_layer: List[nn.Module], 
@@ -124,6 +182,56 @@ class SupervisedSidecarAutoencoder(SidecarAutoencoder):
     return decoding, prediction
 
 
+class SupervisedAutoencoder(Autoencoder):
+
+  def __init__(self, color_channels: int):
+    super().__init__(color_channels)
+
+    fc_layer_size = 16*8*8*color_channels
+    self.supervision = nn.Sequential(
+      FCView(),
+      nn.Linear(in_features=fc_layer_size, out_features=100),
+      nn.Linear(in_features=100, out_features=10),
+    )
+
+  def forward(self, x):
+    upstream = self.upstream_layers(x)
+    encoding = self.encoder(upstream)
+    prediction = self.supervision(encoding)
+    decoding = self.decoder(encoding)
+    return decoding, prediction
+
+
+class RandomMap(nn.Module):
+
+  requires_training: bool = False
+
+  def __init__(self, 
+    in_shape: Tuple[int, int, int], 
+    out_shape: Tuple[int, int, int]
+  ):
+    super().__init__()
+    self.N:int = reduce(lambda a,b: a*b, in_shape)
+    self.M:int = reduce(lambda a,b: a*b, out_shape)
+    
+    self.out_shape = out_shape
+    self.in_shape = in_shape
+
+    P = permutation(np.eye(self.N))
+    P_t = torch.from_numpy(P.astype(np.float32))
+
+    # self.P_st = sparse.IntTensor(P_t)
+    self.P_st = torch.nn.Parameter(P_t)
+    
+    
+
+  def forward(self, x):
+    assert len(x.shape) is 4
+    x_perm = x.view(x.size(0), -1) @ self.P_st
+    x = torch.narrow(x_perm, dim=1, start=0, length=self.M)
+    return x.view([x.size(0), *self.out_shape])
+
+
 class SidecarMap(nn.Module):
 
   requires_training: bool = False
@@ -136,6 +244,85 @@ class SidecarMap(nn.Module):
     x = self.function(x)
     return x
 
+
+
+class InterpolationMap(nn.Module):
+
+  requires_training: bool = False
+  
+  def __init__(self):
+    pass
+
+  def forward(self, x):
+    pass
+
+
+class ConvMap(nn.Module):
+
+  requires_training: bool = True
+
+  def __init__(self,
+    in_shape: Tuple[int, int, int],
+    out_shape: Tuple[int, int, int]
+  ):
+    super().__init__() 
+    
+    bn_args = { "eps":1e-05, "momentum":0.1, "affine":True, "track_running_stats":True }
+    c2d_args = { "kernel_size":3, "stride":1, "padding":1 }
+
+    self.map = nn.Sequential(
+      Interpolate(),
+      nn.Conv2d(in_channels=in_shape[0], out_channels=out_shape[0], **c2d_args), 
+      nn.BatchNorm2d(num_features=out_shape[0], **bn_args),
+      nn.LeakyReLU(True)
+    )
+
+  def forward(self, x):
+    x = self.map(x)
+    return x
+
+
+class DecoderMap(nn.Module):
+
+  requires_training: bool = True
+
+
+  def __init__(self, trained_net: Autoencoder):
+    super().__init__()
+    self.initialized: bool = False
+
+    tensor = trained_net.final_conv2d
+    bn_args = { "eps":1e-05, "momentum":0.1, "affine":True, "track_running_stats":True }
+    
+    self.map = nn.Sequential(
+      Interpolate(),
+      nn.Conv2d(
+        in_channels=tensor.in_channels,
+        out_channels=tensor.out_channels,
+        kernel_size=tensor.kernel_size,
+        stride=tensor.stride,
+        padding=tensor.padding
+      ), 
+      nn.BatchNorm2d(num_features=tensor.out_channels, **bn_args),
+      nn.Dropout(0.3),
+      nn.LeakyReLU(True)
+    )
+
+    # have to store these in an array to not make nn.Modle add
+    # these params to the self.parameters() list,
+    # otherwise they are considedered for optimization which is bad
+    self.fixed_params = {'tensor': tensor}
+
+  def forward(self, x):
+    if not self.initialized:
+      self.initialized = True
+      tensor = self.fixed_params['tensor']
+      state = tensor.state_dict()
+      self.map[1].load_state_dict(state)
+      pass # get map from trained_net
+    x = self.map(x)
+    return x
+    
 
 class NetworkStack(nn.Module):
 
