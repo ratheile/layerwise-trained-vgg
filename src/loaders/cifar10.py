74f8d522fb08094d32819bbb1e828ee033e4e524
Kaan Oktay <kaan.oktay@boun.edu.tr>
2020-01-02 20:20:39 +0100

    Bug fix in loader.

--- src/loaders/cifar10.py
+++ src/loaders/cifar10.py
@@ -28,7 +28,7 @@ def semi_supervised_cifar10(
   download=False,
   augmentation=False,
   num_workers=(6,6,2)
-) -> Tuple[DataLoader, DataLoader, DataLoader]:    
+) -> Tuple[DataLoader, DataLoader, DataLoader]:
 
   light = transforms.Compose([
     transforms.ToTensor(),
@@ -38,7 +38,7 @@ def semi_supervised_cifar10(
   med_30 = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
     transforms.RandomRotation(30),
-    transforms.RandomHorizontalFlip(),                                
+    transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize((0.424, 0.415, 0.384), (0.283, 0.278, 0.284))
   ])
@@ -46,15 +46,15 @@ def semi_supervised_cifar10(
   med_20 = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
     transforms.RandomRotation(20),
-    transforms.RandomHorizontalFlip(),                                
+    transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize((0.424, 0.415, 0.384), (0.283, 0.278, 0.284))
-  ]) 
-  
+  ])
+
   med_10 = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
     transforms.RandomRotation(10),
-    transforms.RandomHorizontalFlip(),                                
+    transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize((0.424, 0.415, 0.384), (0.283, 0.278, 0.284))
   ])
@@ -63,13 +63,14 @@ def semi_supervised_cifar10(
     transforms.RandomCrop(32, padding=4),
     transforms.RandomRotation(45),
     transforms.RandomHorizontalFlip(),
-    transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),                                
+    transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),
     transforms.ToTensor(),
     transforms.Normalize((0.424, 0.415, 0.384), (0.283, 0.278, 0.284)) # TODO: WHY???
   ])
 
-  test_transform = transforms.Compose([                              
-    transforms.ToTensor()
+  test_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.424, 0.415, 0.384), (0.283, 0.278, 0.284))
   ])
 
   transforms_dict = {
@@ -124,25 +125,25 @@ def semi_supervised_cifar10(
     unsupervised_sampler = CifarSubsetSampler(index_total[ss_ds_len:])
     us_bs = int(batch_size - (batch_size * supervised_ratio))
     s_bs = int(batch_size * supervised_ratio)
-    
+
   assert us_bs + s_bs == batch_size
 
   logging.info(f"Dataset (supervised={ss_ds_len}/total={ds_size}) us_bs={us_bs} s_bs={s_bs}")
   logging.info(f"Using workers (unsupervised|supervised|test) {num_workers}")
 
-  unsupervised_loader = DataLoader(ds_train_us, 
+  unsupervised_loader = DataLoader(ds_train_us,
     num_workers=num_workers[0],
     sampler=unsupervised_sampler,
     batch_size=us_bs
   )
 
-  supervised_loader = DataLoader(ds_train_s, 
+  supervised_loader = DataLoader(ds_train_s,
     num_workers=num_workers[1],
     sampler=supervised_sampler,
     batch_size=s_bs
   )
 
-  test_loader = DataLoader(ds_test, 
+  test_loader = DataLoader(ds_test,
     num_workers=num_workers[2],
     batch_size=batch_size
   )
