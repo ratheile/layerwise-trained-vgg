f33d287b9896b76548bb2fc1873202f17f05ac59
Raffael Theiler <rptheiler@protonmail.com>
2020-01-04 18:49:31 +0100

    bsub mod

--- bsub_raffi.sh
+++ bsub_raffi.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd $HOME/scaling-spoon
-bsub -n 4 -R "rusage[mem=16000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceRTX2080Ti]" ./run_raffi.sh "$1"
+# bsub -n 4 -R "rusage[mem=16000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceRTX2080Ti]" ./run_raffi.sh "$1"
+bsub -n 4 -R "rusage[mem=10000,ngpus_excl_p=1]" ./run_raffi.sh "$1"
